90377220640db5bdf634cd1bfbc2894e3b9d302c
htanzeel01 <54932095+htanzeel01@users.noreply.github.com>
2023-02-02 19:37:14 +0100

    commit

--- models.py
+++ models.py
@@ -4,13 +4,12 @@ from marshmallow_sqlalchemy import fields
 
 from config import db, ma, app
 
-    
 
     
 class Student(db.Model):
     __tablename__ = "student"
     id = db.Column(db.Integer, primary_key=True)
-    lname = db.Column(db.String(32), unique=True)
+    lname = db.Column(db.String(32))
     fname = db.Column(db.String(32))
     email = db.Column(db.String(32))
     deleted_at = db.Column(db.String(250),nullable = True)
@@ -51,5 +50,37 @@ classroom_schema = ClassSchema() #1 object
 class_schema = ClassSchema(many=True) #many objects
 student_schema = StudentSchema()
 people_schema = StudentSchema(many=True)
+
+
+PEOPLE_NOTES = [
+    {
+        "classname": "IT4A",
+        "student": [
+            (
+                "--",
+                "brandon@gmail.com",
+                "brandon",
+                "palmer"
+                )                   
+        ],
+    },
+]
+    
+
 with app.app_context():
+    db.drop_all()
     db.create_all()
+    for data in PEOPLE_NOTES:
+        classroom = Classroom(classname=data.get("classname"))
+        for deleted_at,email,fname,lname in data.get("student", []):  
+            classroom.student.append(
+                Student(
+                    deleted_at=deleted_at,
+                    email=email,
+                    fname=fname,
+                    lname=lname
+                    ),
+                )       
+        db.session.add(classroom)
+    db.session.commit()
+
